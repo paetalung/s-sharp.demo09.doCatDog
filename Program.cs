da4a64e2a7f0744910c2388e669995c3085305b8
paetalung <paetalung@gmail.com>
2019-06-10 09:19:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace demo09.doCatDog
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            String input_check = null;
+
+            do
+            {
+                // แสดง แมว
+                Console.WriteLine("Here is a cat.");
+
+                // ถามว่าอยากดูหมาไหม?
+                Console.WriteLine("Do you want to see a dog. (y/n) :");
+                input_check = Console.ReadLine();
+
+                if (input_check == "y")
+                {
+                    Console.WriteLine("Here is a dog.");
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("Do you want to see a cat. (y/n) :");
+                input_check = Console.ReadLine();
+                
+            } while (input_check =="y");
+            Console.WriteLine("\nThanks, See you again.");
+        }
+    }
+}
